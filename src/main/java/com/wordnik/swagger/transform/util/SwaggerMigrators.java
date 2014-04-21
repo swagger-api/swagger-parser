6597f7713c4852745f1809ae7a1a529e4e394e60
Francis Galiegue <fgaliegue@gmail.com>
2014-04-21 19:56:31 +0200

    Start and add javadoc to SwaggerMigrator{,s}

--- src/main/java/com/wordnik/swagger/transform/util/SwaggerMigrators.java
+++ src/main/java/com/wordnik/swagger/transform/util/SwaggerMigrators.java
@@ -11,15 +11,143 @@ import com.wordnik.swagger.transform.migrate.SwaggerMigrator;
 import com.google.common.collect.ImmutableSet;
 
 import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Set;
 
+/**
+ * Utility {@link SwaggerMigrator}s
+ */
+@ParametersAreNonnullByDefault
 public final class SwaggerMigrators
 {
     private static final ObjectMapper MAPPER = JacksonUtils.newMapper();
 
-    private static final class MembersToString
+    /**
+     * Return a migrator converting object member values to JSON Strings
+     *
+     * <p>Note that this will only work if member values are
+     * <strong>not</strong> containers (ie, JSON Arrays or Objects).</p>
+     *
+     * @param first first member name
+     * @param others one or more other member names
+     * @return a migrator
+     *
+     * @see MembersToString
+     */
+    public static SwaggerMigrator membersToString(
+        final String first, final String... others)
+    {
+        return new MembersToString(first, others);
+    }
+
+    /**
+     * Return a migrator applying a <a
+     * href="tools.ietf.org/html/rfc6902">JSON Patch</a>
+     *
+     * <p>The JSON Patch must be deserialized at this point. You can also load
+     * one from the classpath using {@link #patchFromResource(String)}.</p>
+     *
+     * @param patch the JSON patch to apply
+     * @return a migrator
+     *
+     * @see JsonPatch
+     */
+    public static SwaggerMigrator fromPatch(final JsonPatch patch)
+    {
+        Objects.requireNonNull(patch, "patch cannot be null");
+        return new SwaggerMigrator()
+        {
+            @Nonnull
+            @Override
+            public JsonNode migrate(@Nonnull final JsonNode input)
+                throws SwaggerTransformException
+            {
+                Objects.requireNonNull(input);
+                try {
+                    return patch.apply(input);
+                } catch (JsonPatchException e) {
+                    throw new SwaggerTransformException(e);
+                }
+            }
+        };
+    }
+
+    /**
+     * Return a migrator renaming object members
+     *
+     * <p>Note that this migrator will not fail if the member to rename does not
+     * exists; however it <strong>will</strong> fail if the <em>target</em>
+     * member already exists in the target JSON Object.</p>
+     *
+     * @param from the member name to rename
+     * @param to the new name
+     * @return a migrator
+     */
+    public static SwaggerMigrator renameMember(final String from,
+        final String to)
+    {
+        Objects.requireNonNull(from);
+        Objects.requireNonNull(to);
+
+        return new SwaggerMigrator()
+        {
+            @Nonnull
+            @Override
+            public JsonNode migrate(@Nonnull final JsonNode input)
+            {
+                Objects.requireNonNull(input);
+                if (!input.has(from))
+                    return input;
+                if (input.has(to))
+                    throw new UncheckedSwaggerTransformException();
+                final ObjectNode ret = input.deepCopy();
+                ret.put(to, ret.get(from));
+                ret.remove(from);
+                return ret;
+            }
+        };
+    }
+
+    /**
+     * Return a migrator applying a JSON Patch as read from the classpath
+     *
+     * @param resourcePath the resource path
+     * @return a migrator
+     *
+     * @see #fromPatch(JsonPatch)
+     */
+    public static SwaggerMigrator patchFromResource(final String resourcePath)
+    {
+        Objects.requireNonNull(resourcePath);
+
+        final JsonNode node;
+        final JsonPatch patch;
+
+        try {
+            node = JsonLoader.fromResource(resourcePath);
+            patch = MAPPER.readValue(node.traverse(), JsonPatch.class);
+        } catch (IOException e) {
+            throw new RuntimeException("cannot load resource", e);
+        }
+
+        return fromPatch(patch);
+    }
+
+    /**
+     * Migrator converting object member values to JSON Strings
+     *
+     * <p>Important notes:</p>
+     *
+     * <ul>
+     *     <li>this migrator will fail if at least one specified member has a
+     *     "container" value (ie, the value is a JSON Array or an Objet);</li>
+     *     <li>the {@link JsonNode} given as an input is unaltered; a modified
+     *     copy is returned.</li>
+     * </ul>
+     */
+    public static final class MembersToString
         implements SwaggerMigrator
     {
         private final Set<String> memberNames;
@@ -52,76 +180,6 @@ public final class SwaggerMigrators
 
             return ret;
         }
-    }
-
-    public static SwaggerMigrator membersToString(
-        final String first, final String... others)
-    {
-        return new MembersToString(first, others);
-    }
-
-    public static SwaggerMigrator fromPatch(
-        @Nonnull final JsonPatch patch)
-    {
-        Objects.requireNonNull(patch, "patch cannot be null");
-        return new SwaggerMigrator()
-        {
-            @Nonnull
-            @Override
-            public JsonNode migrate(@Nonnull final JsonNode input)
-                throws SwaggerTransformException
-            {
-                Objects.requireNonNull(input);
-                try {
-                    return patch.apply(input);
-                } catch (JsonPatchException e) {
-                    throw new SwaggerTransformException(e);
-                }
-            }
-        };
-    }
-
-    public static SwaggerMigrator renameMember(@Nonnull final String from,
-        @Nonnull final String to)
-    {
-        Objects.requireNonNull(from);
-        Objects.requireNonNull(to);
-
-        return new SwaggerMigrator()
-        {
-            @Nonnull
-            @Override
-            public JsonNode migrate(@Nonnull final JsonNode input)
-            {
-                Objects.requireNonNull(input);
-                if (!input.has(from))
-                    return input;
-                if (input.has(to))
-                    throw new UncheckedSwaggerTransformException();
-                final ObjectNode ret = input.deepCopy();
-                ret.put(to, ret.get(from));
-                ret.remove(from);
-                return ret;
-            }
-        };
-    }
-
-    public static SwaggerMigrator patchFromResource(
-        @Nonnull final String resourcePath)
-    {
-        Objects.requireNonNull(resourcePath);
-
-        final JsonNode node;
-        final JsonPatch patch;
-
-        try {
-            node = JsonLoader.fromResource(resourcePath);
-            patch = MAPPER.readValue(node.traverse(), JsonPatch.class);
-        } catch (IOException e) {
-            throw new RuntimeException("cannot load resource", e);
-        }
-
-        return fromPatch(patch);
     }
 }
 
