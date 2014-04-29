dfa205ab1a218f3f3c05d5156da611e47ef38f0a
Tony Tam <fehguy@gmail.com>
2014-04-29 12:27:14 -0700

    added test

---
+++ src/test/java/com/wordnik/swagger/parser/ResourceListingExtractorTest.java
@@ -0,0 +1,37 @@
+package com.wordnik.swagger.parser;
+
+import com.wordnik.swagger.models.resourcelisting.ResourceListing;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jackson.JacksonUtils;
+
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static org.testng.Assert.*;
+
+public class ResourceListingExtractorTest {
+  @Test
+  public void readResourceListing() throws Exception {
+    String resourceListingUri = "https://api.helloreverb.com/v2/api-docs";
+    ObjectMapper mapper = JacksonUtils.newMapper();
+
+    File resourceListingFile = new File("src/test/resources/specs/v1_2/petstore/api-docs");
+    ResourceListing resourceListing = mapper.readValue(resourceListingFile, ResourceListing.class);
+
+
+    List<ApiListingReference> apis = resourceListing.getApis();
+    for(ApiListingReference ref : apis) {
+      String path = ref.getPath();
+
+      if(path.startsWith("http")) {
+        // absolute listing, do some magic
+      }
+      else {
+        // it is possible that the resource listing URL has a trailing slash and the path has a leading one
+        path = (resourceListingUri + path).replace("//","/");
+      }
+    }
+  }
+}
