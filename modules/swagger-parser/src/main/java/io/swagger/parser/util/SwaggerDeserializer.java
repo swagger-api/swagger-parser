af95bc0cdbab3067a6e8dd84c3dd29263745a663
wing328 <wing328hk@gmail.com>
2015-10-14 16:47:30 +0800

    fix after rebasing master

--- modules/swagger-parser/src/main/java/io/swagger/parser/util/SwaggerDeserializer.java
+++ modules/swagger-parser/src/main/java/io/swagger/parser/util/SwaggerDeserializer.java
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.Collections;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
