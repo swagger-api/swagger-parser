b71917029dc6e8dc95c432fa5adc9c5789ef7ea1
Diego de Estrada <destrada@medallia.com>
2016-07-26 15:00:31 -0300

    Adds failing test for issue 270

--- modules/swagger-parser/src/test/java/io/swagger/parser/SwaggerParserTest.java
+++ modules/swagger-parser/src/test/java/io/swagger/parser/SwaggerParserTest.java
@@ -139,6 +139,16 @@ public class SwaggerParserTest {
     }
 
     @Test
+    public void testLoadExternalNestedDefinitions() throws Exception {
+        SwaggerParser parser = new SwaggerParser();
+        final Swagger swagger = parser.read("src/test/resources/nested-references/b.yaml");
+        Map<String, Model> definitions = swagger.getDefinitions();
+        assertTrue(definitions.containsKey("x"));
+        assertTrue(!definitions.containsKey("y"));
+        assertTrue(definitions.containsKey("z"));
+    }
+
+    @Test
     public void testPetstore() throws Exception {
         SwaggerParser parser = new SwaggerParser();
         SwaggerDeserializationResult result = parser.readWithInfo("src/test/resources/petstore.json", null, true);
