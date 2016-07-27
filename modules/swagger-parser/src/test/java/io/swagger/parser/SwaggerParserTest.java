b0913d4ec556cd1cca0ebe5744dd578d8903924a
Diego de Estrada <destrada@medallia.com>
2016-07-27 14:46:12 -0300

    Adds failing test for issue #274

--- modules/swagger-parser/src/test/java/io/swagger/parser/SwaggerParserTest.java
+++ modules/swagger-parser/src/test/java/io/swagger/parser/SwaggerParserTest.java
@@ -147,6 +147,15 @@ public class SwaggerParserTest {
     }
 
     @Test
+    public void testFileReferenceWithVendorExt() throws Exception {
+        SwaggerParser parser = new SwaggerParser();
+        final Swagger swagger = parser.read("src/test/resources/file-reference-with-vendor-ext/b.yaml");
+        Map<String, Model> definitions = swagger.getDefinitions();
+        assertTrue(definitions.get("x").getVendorExtensions().get("x-foo") instanceof Map);
+        assertTrue(definitions.get("z").getVendorExtensions().get("x-foo") instanceof Map);
+    }
+
+    @Test
     public void testTroublesomeFile() throws Exception {
         SwaggerParser parser = new SwaggerParser();
         final Swagger swagger = parser.read("src/test/resources/troublesome.yaml");
