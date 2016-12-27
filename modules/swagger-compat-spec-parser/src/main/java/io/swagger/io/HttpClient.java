20bcae6ade8e3483cb0c2c151c215317598f692b
Tony Tam <fehguy@gmail.com>
2016-12-26 23:11:42 -0800

    added debugs

--- modules/swagger-compat-spec-parser/src/main/java/io/swagger/io/HttpClient.java
+++ modules/swagger-compat-spec-parser/src/main/java/io/swagger/io/HttpClient.java
@@ -5,6 +5,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,6 +15,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpClient {
+    static Logger LOGGER = LoggerFactory.getLogger(HttpClient.class);
+
     private String baseUrl;
     private Map<String, String> queryParams = new HashMap<>();
     private Map<String, String> headers = new HashMap<>();
@@ -53,13 +57,13 @@ public class HttpClient {
         try {
             response.close();
         } catch (IOException | NullPointerException e) {
-            e.printStackTrace();
+            LOGGER.error("failed to close", e);
         }
 
         try {
             httpClient.close();
         } catch (IOException | NullPointerException e) {
-            e.printStackTrace();
+            LOGGER.error("failed to close", e);
         }
     }
 }
