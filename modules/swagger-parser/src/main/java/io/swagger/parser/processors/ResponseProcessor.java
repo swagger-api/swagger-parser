a3eaf93b0cbc7ad802d3726ec60e8750a9727f88
Tom Howard <tom@windyroad.com.au>
2017-03-08 16:01:50 +1100

    Added support for $ref Headers
    We shouldn’t have to repeat the definitions for headers in every single
    response.
    
    Please don’t make me repeat the header definitions in every response
    (ಥ﹏ಥ)
    
    Please see modules/swagger-parser/src/test/resources/headers.yaml for
    an example
    
    @Helmsdown what’s your view? From what I can tell, you originally made
    the comment that it wasn’t needed. If so, your feedback (positive or
    negative) would be greatly appreciated.

--- modules/swagger-parser/src/main/java/io/swagger/parser/processors/ResponseProcessor.java
+++ modules/swagger-parser/src/main/java/io/swagger/parser/processors/ResponseProcessor.java
@@ -1,30 +1,62 @@
 package io.swagger.parser.processors;
 
+import java.util.Map;
+
+import io.swagger.models.RefResponse;
 import io.swagger.models.Response;
 import io.swagger.models.Swagger;
 import io.swagger.models.properties.Property;
+import io.swagger.models.properties.RefProperty;
 import io.swagger.parser.ResolverCache;
 
 public class ResponseProcessor {
 
     private final PropertyProcessor propertyProcessor;
+    private final ResolverCache cache;
 
     public ResponseProcessor(ResolverCache cache, Swagger swagger) {
+        this.cache = cache;
         propertyProcessor = new PropertyProcessor(cache, swagger);
     }
 
     public void processResponse(Response response) {
-        //process the response body
+        // process the response body
         final Property schema = response.getSchema();
 
         if (schema != null) {
             propertyProcessor.processProperty(schema);
         }
 
-        /* intentionally ignoring the response headers, even those these were modelled as a
-         Map<String, Property> they should never have a $ref because what does it mean to have a
-         complex object in an HTTP header?
-          */
+        /*
+         * intentionally ignoring the response headers, even those these were
+         * modelled as a Map<String, Property> they should never have a $ref
+         * because what does it mean to have a complex object in an HTTP header?
+         * 
+         * @tompahoward: Having $ref in headers doesn't mean they are complex
+         * objects, it means that we don't want to repeat the definition for
+         * each header in every response. We should be able to define a header
+         * once and $ref refer to it everywhere we ant to use it.
+         */
 
+        final Map<String, Property> headers = response.getHeaders();
+        if (headers != null) {
+            for (String headerName : headers.keySet()) {
+                Property header = headers.get(headerName);
+
+                if (header != null) {
+                    if (header instanceof RefProperty) {
+                        RefProperty refHeader = (RefProperty) header;
+                        Property resolvedHeader = cache.loadRef(refHeader.get$ref(), refHeader.getRefFormat(),
+                                Property.class);
+
+                        if (resolvedHeader != null) {
+                            header = resolvedHeader;
+                            headers.put(headerName, resolvedHeader);
+                        }
+                    }
+                    propertyProcessor.processProperty(header);
+                }
+            }
+        }
     }
 }
