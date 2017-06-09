7fb73dd801433c3ae70efb50c78c3495f9dbf003
gracekarina <>
2017-06-09 16:54:15 -0500

    Improvements on the PR

--- modules/swagger-parser/src/main/java/io/swagger/parser/OpenApiResolver.java
+++ modules/swagger-parser/src/main/java/io/swagger/parser/OpenApiResolver.java
@@ -4,7 +4,7 @@ import io.swagger.oas.models.Operation;
 import io.swagger.oas.models.PathItem;
 import io.swagger.oas.models.Paths;
 import io.swagger.oas.models.OpenAPI;
-import io.swagger.models.auth.AuthorizationValue;
+//import io.swagger.models.auth.AuthorizationValue;
 import io.swagger.parser.processors.DefinitionsProcessor;
 import io.swagger.parser.processors.OperationProcessor;
 import io.swagger.parser.processors.PathsProcessor;
@@ -15,55 +15,55 @@ import java.util.List;
  */
 public class OpenApiResolver {
 
-        private final OpenAPI openApi;
-        private final ResolverCache cache;
-        private final PathsProcessor pathProcessor;
-        private final DefinitionsProcessor definitionsProcessor;
-        private final OperationProcessor operationsProcessor;
-        private Settings settings = new Settings();
+    /*private final OpenAPI openApi;
+    private final ResolverCache cache;
+    private final PathsProcessor pathProcessor;
+    private final DefinitionsProcessor definitionsProcessor;
+    private final OperationProcessor operationsProcessor;
+    private Settings settings = new Settings();
 
-        public OpenApiResolver(OpenAPI openApi) {
-            this(openApi, null, null, null);
+    public OpenApiResolver(OpenAPI openApi) {
+        this(openApi, null, null, null);
+    }
+
+    public OpenApiResolver(OpenAPI openApi,  List<AuthorizationValue> auths) {
+        this(openApi, auths, null, null);
+    }
+
+    public OpenApiResolver(OpenAPI openApi, List<AuthorizationValue> auths, String parentFileLocation) {
+        this(openApi, auths, parentFileLocation, null);
+    }
+
+    public OpenApiResolver(OpenAPI openApi, List<AuthorizationValue> auths, String parentFileLocation, Settings settings) {
+        this.openApi = openApi;
+        this.settings = settings != null ? settings : new Settings();
+        this.cache = new ResolverCache(openApi, auths, parentFileLocation);
+        definitionsProcessor = new DefinitionsProcessor(cache, swagger);
+        pathProcessor = new PathsProcessor(cache, swagger, this.settings);
+        operationsProcessor = new OperationProcessor(cache, swagger);
+    }
+
+    public OpenAPI resolve() {
+        if (openApi == null) {
+            return null;
         }
 
-        public OpenApiResolver(OpenAPI openApi,  List<AuthorizationValue> auths) {
-            this(openApi, auths, null, null);
-        }
+        pathProcessor.processPaths();
+        definitionsProcessor.processDefinitions();
 
-        public OpenApiResolver(OpenAPI openApi, List<AuthorizationValue> auths, String parentFileLocation) {
-            this(openApi, auths, parentFileLocation, null);
-        }
-
-        public OpenApiResolver(OpenAPI openApi, List<AuthorizationValue> auths, String parentFileLocation, Settings settings) {
-            this.openApi = openApi;
-            this.settings = settings != null ? settings : new Settings();
-            this.cache = new ResolverCache(openApi, auths, parentFileLocation);
-            definitionsProcessor = new DefinitionsProcessor(cache, swagger);
-            pathProcessor = new PathsProcessor(cache, swagger, this.settings);
-            operationsProcessor = new OperationProcessor(cache, swagger);
-        }
-
-        public OpenAPI resolve() {
-            if (openApi == null) {
-                return null;
-            }
-
-            pathProcessor.processPaths();
-            definitionsProcessor.processDefinitions();
-
-            if(openApi.getPaths() != null) {
-                for(String pathname : openApi.getPaths().keySet()) {
-                    PathItem pathItem = openApi.getPaths().get(pathname);
-                    if(pathItem.getOperations() != null) {
-                        for(Operation operation : paths.getOperations()) {
-                            operationsProcessor.processOperation(operation);
-                        }
+        if(openApi.getPaths() != null) {
+            for(String pathname : openApi.getPaths().keySet()) {
+                PathItem pathItem = openApi.getPaths().get(pathname);
+                if(pathItem.getOperations() != null) {
+                    for(Operation operation : paths.getOperations()) {
+                        operationsProcessor.processOperation(operation);
                     }
                 }
             }
-
-            return openApi;
         }
+
+        return openApi;
+    }*/
 
         public static class Settings {
 
@@ -87,4 +87,4 @@ public class OpenApiResolver {
 
         }
     }
-}
+//}
