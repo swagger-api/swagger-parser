e5328df874140cc78b608581c6275a896a0bffc5
Ron <webron@users.noreply.github.com>
2017-09-19 12:09:19 +0300

    remove empty servers

--- modules/swagger-parser-v2-converter/src/main/java/io/swagger/parser/v2/SwaggerConverter.java
+++ modules/swagger-parser-v2-converter/src/main/java/io/swagger/parser/v2/SwaggerConverter.java
@@ -379,6 +379,8 @@ public class SwaggerConverter implements SwaggerParserExtension {
                 server.setUrl(baseUrl);
 
                 servers.add(server);
+            } else {
+                return null;
             }
         }
 
