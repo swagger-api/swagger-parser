f8e58d7afc9fa9207f951b6d087ada047cc83064
gracekarina <gracekarina.gonzalez@gmail.com>
2017-10-17 11:54:27 -0500

    removing commented code

--- modules/swagger-parser/src/main/java/io/swagger/parser/util/SwaggerDeserializer.java
+++ modules/swagger-parser/src/main/java/io/swagger/parser/util/SwaggerDeserializer.java
@@ -831,8 +831,6 @@ public class SwaggerDeserializer {
                     result.extra(location, key, node.get(key));
                 }
             }
-            /*if("{ }".equals(Json.pretty(impl)))
-                return null;*/
             model = impl;
         }
         JsonNode exampleNode = node.get("example");
