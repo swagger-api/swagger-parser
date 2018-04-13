de62b78feca41b5743469eb7bc5f0e437056a9f5
gracekarina <gracekarina.gonzalez@gmail.com>
2018-04-12 20:58:52 -0500

    fixing issue #684 - added test and solution to avoid NPE

--- modules/swagger-parser/src/main/java/io/swagger/parser/processors/ParameterProcessor.java
+++ modules/swagger-parser/src/main/java/io/swagger/parser/processors/ParameterProcessor.java
@@ -44,10 +44,12 @@ public class ParameterProcessor {
                 // if the parameter exists, replace it
                 boolean matched = false;
                 for(Parameter param : processedPathLevelParameters) {
-                    if(param.getName().equals(resolvedParameter.getName())) {
-                        // ref param wins
-                        matched = true;
-                        break;
+                    if (param.getName() != null) {
+                        if (param.getName().equals(resolvedParameter.getName())) {
+                            // ref param wins
+                            matched = true;
+                            break;
+                        }
                     }
                 }
                 for(Parameter param : parameters) {
