5fed2c37756b6bb0bdf29766c025c5d5a930e7c0
Mohammed Rizwan <y.mohdriz@gmail.com>
2018-07-02 18:33:33 +0530

    Revert my changes

--- modules/swagger-parser-v2-converter/src/main/java/io/swagger/v3/parser/converter/SwaggerConverter.java
+++ modules/swagger-parser-v2-converter/src/main/java/io/swagger/v3/parser/converter/SwaggerConverter.java
@@ -317,10 +317,7 @@ public class SwaggerConverter implements SwaggerParserExtension {
         }
 
         Scopes scopes = new Scopes();
-        Map<String, String> oAuth2Scopes = oAuth2Definition.getScopes();
-        if (oAuth2Scopes != null) {
-            oAuth2Scopes.forEach((k, v) -> scopes.addString(k, v));
-        }
+        oAuth2Definition.getScopes().forEach((k, v) -> scopes.addString(k, v));
         oAuthFlow.setScopes(scopes);
 
         securityScheme.setFlows(oAuthFlows);
@@ -1061,15 +1058,11 @@ public class SwaggerConverter implements SwaggerParserExtension {
             result = arraySchema;
         } else if (v2Model instanceof ComposedModel) {
             ComposedModel composedModel = (ComposedModel) v2Model;
-            ComposedSchema composed = new ComposedSchema();
-            composed.setDescription(composedModel.getDescription());
-            composed.setExample(composedModel.getExample());
-            if (composedModel.getExternalDocs() != null) {
-                composed.setExternalDocs(convert(composedModel.getExternalDocs()));
-            }
-            composed.setTitle(composedModel.getTitle());
-            composed.setExtensions(convert(composedModel.getVendorExtensions()));
+
+            ComposedSchema composed = Json.mapper().convertValue(v2Model, ComposedSchema.class);
+
             composed.setAllOf(composedModel.getAllOf().stream().map(this::convert).collect(Collectors.toList()));
+
             result = composed;
         } else {
             String v2discriminator = null;
