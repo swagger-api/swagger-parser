66990c1cf8af6e6e19da586817e6ab0fcca74630
gracekarina <gracekarina.gonzalez@gmail.com>
2021-07-24 17:43:07 -0500

    allow to filter authorization by URL

--- modules/swagger-parser/src/main/java/io/swagger/parser/util/RemoteUrl.java
+++ modules/swagger-parser/src/main/java/io/swagger/parser/util/RemoteUrl.java
@@ -101,10 +101,12 @@ public class RemoteUrl {
                 final List<AuthorizationValue> header = new ArrayList<>();
                 if (auths != null) {
                     for (AuthorizationValue auth : auths) {
-                        if ("query".equals(auth.getType())) {
-                            appendValue(inUrl, auth, query);
-                        } else if ("header".equals(auth.getType())) {
-                            appendValue(inUrl, auth, header);
+                        if (auth.getUrls() == null || auth.getUrls().contains(inUrl)) {
+                            if ("query".equals(auth.getType())) {
+                                appendValue(inUrl, auth, query);
+                            } else if ("header".equals(auth.getType())) {
+                                appendValue(inUrl, auth, header);
+                            }
                         }
                     }
                 }
