b745034032beac54f60eac0375c50d44ee19ab96
gracekarina <gracekarina.gonzalez@gmail.com>
2021-09-28 21:34:45 -0500

    oas 3.1 - info.summary - components.pathItem - discriminator.extension

--- modules/swagger-parser-v3/src/test/java/io/swagger/v3/parser/test/OAI31DeserializationTest.java
+++ modules/swagger-parser-v3/src/test/java/io/swagger/v3/parser/test/OAI31DeserializationTest.java
@@ -1,5 +1,6 @@
 package io.swagger.v3.parser.test;
 
+import io.swagger.v3.core.util.Yaml;
 import io.swagger.v3.core.util.Yaml31;
 import io.swagger.v3.parser.OpenAPIV3Parser;
 import io.swagger.v3.parser.core.models.ParseOptions;
@@ -7,6 +8,7 @@ import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertFalse;
 
 public class OAI31DeserializationTest {
     @Test
@@ -31,7 +33,6 @@ public class OAI31DeserializationTest {
         ParseOptions options = new ParseOptions();
         options.setResolve(true);
         SwaggerParseResult result = new OpenAPIV3Parser().readContents(json, null, options);
-
         assertNotNull(result.getOpenAPI());
     }
 
@@ -41,5 +42,46 @@ public class OAI31DeserializationTest {
         Yaml31.prettyPrint(result);
         //assertEquals(result.getMessages().size(),1);
         assertNotNull(result.getOpenAPI());
+    }
+
+    @Test
+    public void testInfo() {
+        String infoYaml = "openapi: 3.1.0\n" +
+                "info:\n" +
+                "  title: Swagger Petstore\n" +
+                "  summary: test summary in info object\n" +
+                "  description: \"This is a sample server Petstore server. You can find out more about\\\n" +
+                "    \\ Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).\\\n" +
+                "    \\ For this sample, you can use the api key `special-key` to test the authorization\\\n" +
+                "    \\ filters.\"\n" +
+                "  termsOfService: http://swagger.io/terms/\n" +
+                "  contact:\n" +
+                "    email: apiteam@swagger.io\n" +
+                "  license:\n" +
+                "    name: Apache 2.0\n" +
+                "    url: http://www.apache.org/licenses/LICENSE-2.0.html\n" +
+                "  version: 1.0.0\n" +
+                "servers:\n" +
+                "- url: /\n";
+        SwaggerParseResult result = new OpenAPIV3Parser().readContents( infoYaml, null, null);
+        assertNotNull(result.getOpenAPI());
+        assertNotNull(result.getOpenAPI().getInfo().getSummary());
+        assertFalse(result.getMessages().contains("attribute info.summary is unexpected"));
+    }
+
+    @Test
+    public void testPathsItemsUnderComponents() {
+        SwaggerParseResult result = new OpenAPIV3Parser().readLocation( "3.1.0/petstore-3.1_more.yaml", null, null);
+        assertNotNull(result.getOpenAPI());
+        assertNotNull(result.getOpenAPI().getComponents().getPathItems());
+        assertFalse(result.getMessages().contains("attribute components.pathItems is unexpected"));
+
+    }
+
+    @Test
+    public void testDiscriminatorExtensions() {
+        SwaggerParseResult result = new OpenAPIV3Parser().readLocation( "3.1.0/petstore-3.1_more.yaml", null, null);
+        assertNotNull(result.getOpenAPI());
+        assertNotNull(result.getOpenAPI().getComponents().getSchemas().get("DiscriminatorExtension").getDiscriminator().getExtensions().get("x-extension"));
     }
 }
