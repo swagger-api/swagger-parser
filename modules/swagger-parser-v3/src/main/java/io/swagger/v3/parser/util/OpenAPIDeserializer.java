fe6017ee42a01328fd5b7d52f0f070c7ea9fc5b8
gracekarina <gracekarina.gonzalez@gmail.com>
2021-10-08 13:08:45 -0500

    oas 3.1 - minor fix

--- modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/util/OpenAPIDeserializer.java
+++ modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/util/OpenAPIDeserializer.java
@@ -268,6 +268,10 @@ public class OpenAPIDeserializer {
     private Map<String,String> localSchemaRefs = new HashMap<>();
 
 	public SwaggerParseResult deserialize(JsonNode rootNode) {
+		return deserialize(rootNode, null);
+	}
+
+	public SwaggerParseResult deserialize(JsonNode rootNode, String path) {
 		return deserialize(rootNode, null, false);
 	}
 
@@ -395,7 +399,7 @@ public class OpenAPIDeserializer {
 			if (result.isOpenapi31()) {
 				value = getString("jsonSchemaDialect", rootNode, false, location, result);
 				if (value != null) {
-					if (isValidURL(value)) {
+					if (isValidURI(value)) {
 						openAPI.setJsonSchemaDialect(value);
 					}else{
 						result.warning(location,"jsonSchemaDialect. Invalid url: " + value);
@@ -423,6 +427,15 @@ public class OpenAPIDeserializer {
 			return null;
 		}
 		return openAPI;
+	}
+
+	boolean isValidURI(String uriString) {
+		try {
+			URI uri = new URI(uriString);
+			return true;
+		} catch (Exception exception) {
+			return false;
+		}
 	}
 
 	private void validateReservedKeywords(Map<String, Set<String>> specKeys, String key, String location, ParseResult result) {
