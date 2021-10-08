f9a41fc37c05ffa40680fea948d89391850e72f1
gracekarina <gracekarina.gonzalez@gmail.com>
2021-10-08 12:02:20 -0500

    oas 3.1 - test for siblings ref ticket

--- modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/util/OpenAPIDeserializer.java
+++ modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/util/OpenAPIDeserializer.java
@@ -944,7 +944,6 @@ public class OpenAPIDeserializer {
 					if (StringUtils.isNotBlank(value)) {
 						pathItem.setSummary(value);
 					}
-
 					value = getString("description", obj, false, location, result);
 					if (StringUtils.isNotBlank(value)) {
 						pathItem.setDescription(value);
@@ -2156,9 +2155,7 @@ public class OpenAPIDeserializer {
 		return header;
 	}
 
-
 	public Object getAnyExample(String nodeKey, ObjectNode node, String location, ParseResult result) {
-		//TODO: Examples now allows Ref
 		JsonNode example = node.get(nodeKey);
 		if (example != null) {
 			if (example.getNodeType().equals(JsonNodeType.STRING)) {
@@ -2231,7 +2228,6 @@ public class OpenAPIDeserializer {
 		if (node == null) {
 			return null;
 		}
-
 		SecurityScheme securityScheme = new SecurityScheme();
 
 		JsonNode ref = node.get("$ref");
@@ -3118,18 +3114,6 @@ public class OpenAPIDeserializer {
 				} else {
 					example.set$ref(ref.textValue());
 				}
-				if (result.isOpenapi31()) {
-					String desc = getString("summary", node, false, location, result);
-					if (StringUtils.isNotBlank(desc)) {
-						example.setSummary(desc);
-					}
-				}
-				if (result.isOpenapi31()) {
-					String desc = getString("description", node, false, location, result);
-					if (StringUtils.isNotBlank(desc)) {
-						example.setDescription(desc);
-					}
-				}
 				return example;
 			} else {
 				result.invalidType(location, "$ref", "string", node);
@@ -3260,8 +3244,7 @@ public class OpenAPIDeserializer {
 					apiResponse.set$ref(ref.textValue());
 				}
 				if(result.isOpenapi31()){
-					String value = getString("summary", node, false, location, result);
-					value = getString("description", node, false, location, result);
+					String value = getString("description", node, false, location, result);
 					if (StringUtils.isNotBlank(value)) {
 						apiResponse.setDescription(value);
 					}
