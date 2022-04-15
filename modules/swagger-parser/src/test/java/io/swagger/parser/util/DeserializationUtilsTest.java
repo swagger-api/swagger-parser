7570ba072a34d97c6c7d04845d0de57b1b456fa3
mateusz.poliwoczak <m.poliwoczak@gmail.com>
2022-04-15 09:05:40 +0200

    Updated tests with OAS3, added additional assertions for true/false strings.

--- modules/swagger-parser/src/test/java/io/swagger/parser/util/DeserializationUtilsTest.java
+++ modules/swagger-parser/src/test/java/io/swagger/parser/util/DeserializationUtilsTest.java
@@ -9,18 +9,50 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertNotSame;
 
 public class DeserializationUtilsTest {
 
     @Test
-    public void testEnumValuesAreNotConvertedToBooleans() throws IOException {
-        String yaml = readFile("src/test/resources/EnumYesNoOnOff.yaml");
+    public void testEnumValuesAreNotConvertedToBooleansOAS2() throws IOException {
+        String yaml = readFile("src/test/resources/EnumYesNoOnOffOAS2.yaml");
 
         JsonNode jsonNode = DeserializationUtils.readYamlTree(yaml, null);
 
+        jsonNode.findPath("EnumWithQuotes").get("enum").elements()
+                .forEachRemaining(element -> {
+                    assertFalse(element.isBoolean());
+                    assertNotSame(element.textValue(), "true");
+                    assertNotSame(element.textValue(), "false");
+                });
         jsonNode.findPath("EnumNoQuotes").get("enum").elements()
-                .forEachRemaining(element -> assertFalse(element.isBoolean()));
+                .forEachRemaining(element -> {
+                    assertFalse(element.isBoolean());
+                    assertNotSame(element.textValue(), "true");
+                    assertNotSame(element.textValue(), "false");
+                });
     }
+
+    @Test
+    public void testEnumValuesAreNotConvertedToBooleansOAS3() throws IOException {
+        String yaml = readFile("src/test/resources/EnumYesNoOnOffOAS3.yaml");
+
+        JsonNode jsonNode = DeserializationUtils.readYamlTree(yaml, null);
+
+        jsonNode.findPath("EnumWithQuotes").get("enum").elements()
+                .forEachRemaining(element -> {
+                    assertFalse(element.isBoolean());
+                    assertNotSame(element.textValue(), "true");
+                    assertNotSame(element.textValue(), "false");
+                });
+        jsonNode.findPath("EnumNoQuotes").get("enum").elements()
+                .forEachRemaining(element -> {
+                    assertFalse(element.isBoolean());
+                    assertNotSame(element.textValue(), "true");
+                    assertNotSame(element.textValue(), "false");
+                });
+    }
+
 
     private String readFile(String name) throws IOException {
         return new String(Files.readAllBytes(new File(name).toPath()), StandardCharsets.UTF_8);
