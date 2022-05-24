d4d6f83fb00cd858b9e99241b3023e9c489edf15
frantuma <frantuma@yahoo.com>
2022-05-24 14:36:15 +0200

    oas 3.1 dereference - remove anchor merge

--- modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/reference/OpenAPI31Traverser.java
+++ modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/reference/OpenAPI31Traverser.java
@@ -1056,9 +1056,6 @@ public class OpenAPI31Traverser implements Traverser {
         if (source.getMaxLength() != null){
             target.setMaxLength(source.getMaxLength());
         }
-        if (source.get$anchor() != null){
-            target.set$anchor(source.get$anchor());
-        }
         if (source.get$comment() != null){
             target.set$comment(source.get$comment());
         }
