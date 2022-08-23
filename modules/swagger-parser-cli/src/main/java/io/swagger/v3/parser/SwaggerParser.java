510eda5b0c8f4188e0c296f4a9974152b5b01ea3
gracekarina <gracekarina.gonzalez@gmail.com>
2022-08-23 12:28:26 -0500

    validate java version before build

--- modules/swagger-parser-cli/src/main/java/io/swagger/v3/parser/SwaggerParser.java
+++ modules/swagger-parser-cli/src/main/java/io/swagger/v3/parser/SwaggerParser.java
@@ -1,31 +1,123 @@
 package io.swagger.v3.parser;
 
+import io.swagger.v3.core.util.Yaml;
+import io.swagger.v3.parser.core.models.ParseOptions;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.impl.Arguments;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class SwaggerParser {
+
     public static void main(String[] args) {
         if (args.length > 0){
-            List<String> messages = readFromLocation(args[0]);
-            if ( messages.size() > 0){
-                messages.forEach(System.out::println);
+            ArgumentParser parser = ArgumentParsers.newFor("swagger-parser").build()
+                    .defaultHelp(true);
+            parser.addArgument("-i")
+                    .dest("i")
+                    .required(true)
+                    .type(String.class)
+                    .help("input file to be parsed");
+            parser.addArgument("-resolve")
+                    .dest("resolve")
+                    .type(Boolean.class)
+                    .action(Arguments.storeTrue())
+                    .setDefault(false)
+                    .help("resolve remote or local references");
+            parser.addArgument("-resolveFully")
+                    .dest("resolvefully")
+                    .type(Boolean.class)
+                    .action(Arguments.storeTrue())
+                    .setDefault(false)
+                    .help("");
+            parser.addArgument("-flatten")
+                    .dest("flatten")
+                    .type(Boolean.class)
+                    .action(Arguments.storeTrue())
+                    .setDefault(false)
+                    .help("");
+            parser.addArgument("-o")
+                    .dest("o")
+                    .type(String.class)
+                    .help("output file parsed");
+            parser.addArgument("-l")
+                    .dest("l")
+                    .type(String.class)
+                    .help("output error logs");
+            try{
+                readFromLocation(parser.parseArgs(args));
+            }catch (ArgumentParserException e) {
+                parser.handleError(e);
                 System.exit(1);
             }
         }
     }
 
-    public static List<String> readFromLocation(String location) {
+    private static void generateMessagesFile(List<String> messages, Namespace arguments) {
+        if ( messages != null && !messages.isEmpty() && arguments != null && arguments.getString("l") != null){
+            if(arguments.getString("l") != null) {
+                generateParsedFile(arguments, "l", messages.toString());
+            }
+        }
+    }
+
+    public static List<String> readFromLocation(Namespace args) {
         List<String> messages = new ArrayList<>();
+        ParseOptions options = null;
         try {
-            final SwaggerParseResult result = new OpenAPIV3Parser().readLocation(location, null, null);
+            options = setOptions(args);
+            final SwaggerParseResult result = new OpenAPIV3Parser().readLocation(args.get("i"), null, options);
+            if(args.getString("o") != null) {
+                if (result.getOpenAPI() != null){
+                    generateParsedFile(args, "o", Yaml.pretty(result.getOpenAPI()));
+                }
+            }
             if(result.getOpenAPI() == null || !result.getMessages().isEmpty()){
                 messages = result.getMessages();
+                generateMessagesFile(messages, args);
             }
         }catch (Exception e){
             e.printStackTrace();
-            System.exit(1);
         }
         return messages;
+    }
+
+    private static void generateParsedFile(Namespace args, String o, String result) {
+        try {
+            if(result != null) {
+                OutputStream out = Files.newOutputStream(Paths.get(args.getString(o)));
+                byte[] specBytes = result.getBytes();
+                out.write(specBytes);
+                out.close();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static ParseOptions setOptions(Namespace parseOptions) {
+        ParseOptions options  = new ParseOptions();
+
+        if (parseOptions.getString("resolve") !=null && parseOptions.getString("resolve").equals("true")) {
+            options.setResolve(true);
+        }
+        if (parseOptions.getString("resolvefully") != null && parseOptions.getString("resolvefully").equals("true")) {
+            options.setResolveFully(true);
+        }
+        if (parseOptions.getString("flatten") != null && parseOptions.getString("flatten").equals("true")) {
+            options.setFlatten(true);
+        }
+        return options;
     }
 }
--- modules/swagger-parser-cli/src/test/java/SwaggerParserCLITest.java
+++ modules/swagger-parser-cli/src/test/java/SwaggerParserCLITest.java
@@ -1,28 +1,99 @@
 import io.swagger.v3.parser.SwaggerParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SwaggerParserCLITest {
     @Test
     public void validateOKFromLocationTest(){
-        String []args = new String[1];
-        args[0] = "src/test/resources/fileWithNoErrorMessages.yaml";
-        Assert.assertTrue(SwaggerParser.readFromLocation(args[0]).size() == 0);
+        Map<String,Object> args = new HashMap();
+        args.put("i","src/test/resources/fileWithNoErrorMessages.yaml");
+        Namespace namespace = new Namespace(args);
+        Assert.assertTrue(SwaggerParser.readFromLocation(namespace).size() == 0);
     }
 
     @Test
     public void validateErrorFromLocationTest(){
-        String []args = new String[1];
-        args[0] = "src/test/resources/fileWithValidationErrorMessages.yaml";
-        Assert.assertEquals(SwaggerParser.readFromLocation(args[0]).get(0), "attribute info.version is missing");
-        Assert.assertEquals(SwaggerParser.readFromLocation(args[0]).get(1), "attribute paths.'/cu'(post).responses.200.description is missing");
+        Map<String,Object> args = new HashMap();
+        args.put("i","src/test/resources/fileWithValidationErrorMessages.yaml");
+        Namespace namespace = new Namespace(args);
+        Assert.assertEquals(SwaggerParser.readFromLocation(namespace).toString(), "[attribute info.version is missing, attribute paths.'/cu'(post).responses.200.description is missing]");
+    }
+
+    @Test
+    public void validateErrorTest(){
+        Map<String,Object> args = new HashMap();
+        args.put("i","src/test/resources/fileWithValidationErrorMessages.yaml");
+        args.put("resolve", "true");
+        args.put("resolvefully", "true");
+        args.put("o", "target/test-classes/parsedSpec.yaml");
+        args.put("l", "target/test-classes/errorLogs.yaml");
+        Namespace namespace = new Namespace(args);
+        Assert.assertEquals(SwaggerParser.readFromLocation(namespace).toString(), "[attribute info.version is missing, attribute paths.'/cu'(post).responses.200.description is missing]");
     }
 
     @Test
     public void validateFileNotFoundInLocationTest(){
-        String []args = new String[1];
-        args[0] = "src/test/resources/WrongLocation.yaml";
-        Assert.assertTrue(SwaggerParser.readFromLocation(args[0]).size() == 1);
-        Assert.assertEquals(SwaggerParser.readFromLocation(args[0]).get(0), "Unable to read location `src/test/resources/WrongLocation.yaml`");
+        Map<String,Object> args = new HashMap();
+        args.put("i","src/test/resources/WrongLocation.yaml");
+        args.put("l", "target/test-classes/errorLogs.yaml");
+        Namespace namespace = new Namespace(args);
+        List<String> messages = new ArrayList<>();
+        try {
+            messages   = SwaggerParser.readFromLocation(namespace);
+        }catch (Exception e){
+            Assert.fail("error");
+        }
+        Assert.assertTrue( messages.size() == 1);
+        Assert.assertEquals(messages.toString(), "[Unable to read location `src/test/resources/WrongLocation.yaml`]");
+    }
+
+    @Test
+    public void validateOKFromLocationWithResolveOptionTest(){
+        Map<String,Object> args = new HashMap();
+        args.put("i","src/test/resources/internal-references-in-external-files/main.yaml");
+        args.put("resolve", "true");
+        Namespace namespace = new Namespace(args);
+        Assert.assertTrue(SwaggerParser.readFromLocation(namespace).size() == 0);
+    }
+
+    @Test
+    public void validateOKFromLocationWithResolveFullyOptionTest(){
+        Map<String,Object> args = new HashMap();
+        args.put("i","src/test/resources/internal-references-in-external-files/main.yaml");
+        args.put("resolve", "true");
+        args.put("resolvefully", "true");
+        args.put("o", "target/test-classes/parsedSpec.yaml");
+        Namespace namespace = new Namespace(args);
+
+        Assert.assertTrue(SwaggerParser.readFromLocation(namespace).size() == 0);
+    }
+
+    @Test
+    public void validateOKFromLocationWithOnlyResolveFullyOptionTest(){
+        Map<String,Object> args = new HashMap();
+        args.put("i","src/test/resources/internal-references-in-external-files/main.yaml");
+        args.put("resolvefully","true");
+        Namespace namespace = new Namespace(args);
+        Assert.assertTrue(SwaggerParser.readFromLocation(namespace).size() == 0);
+    }
+
+    @Test
+    public void validateOKFromLocationWithFlattenOptionTest() throws ArgumentParserException {
+        Map<String,Object> args = new HashMap();
+        args.put("i","src/test/resources/internal-references-in-external-files/main.yaml");
+        args.put("resolve", "true");
+        args.put("resolvefully", "true");
+        args.put("flatten", "true");
+        args.put("o", "target/test-classes/parsedSpec.yaml");
+        args.put("l", "target/test-classes/errorLogs.yaml");
+        Namespace namespace = new Namespace(args);
+        Assert.assertTrue(SwaggerParser.readFromLocation(namespace).size() == 0);
     }
 }
