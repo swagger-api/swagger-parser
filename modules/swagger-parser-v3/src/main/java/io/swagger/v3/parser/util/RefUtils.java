5907907f7a12d8c58196da0ac8978f0835c7e201
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2022-10-06 14:17:31 +0100

    ISSUE-1745 - Change the buildUrl to handle more cases of path traversal

--- modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/util/RefUtils.java
+++ modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/util/RefUtils.java
@@ -11,6 +11,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -150,48 +151,20 @@ public class RefUtils {
     }
 
     public static String buildUrl(String rootPath, String relativePath) {
-        String[] rootPathParts = rootPath.split("/");
-        String [] relPathParts = relativePath.split("/");
+      if(rootPath == null || relativePath == null) {
+          return null;
+      }
 
-        if(rootPath == null || relativePath == null) {
-            return null;
-        }
-
-        int trimRoot = 0;
-        int trimRel = 0;
-
-        if(!"".equals(rootPathParts[rootPathParts.length - 1])) {
-            trimRoot = 1;
-        }
-        if("".equals(relPathParts[0])) {
-            trimRel = 1; trimRoot = rootPathParts.length-3;
-        }        
-        for(int i = 0; i < rootPathParts.length; i++) {
-            if("".equals(rootPathParts[i])) {
-                trimRel += 1;
-            }
-            else {
-                break;
-            }
-        }
-        for(int i = 0; i < relPathParts.length; i ++) {
-            if(".".equals(relPathParts[i])) {
-                trimRel += 1;
-            }
-            else if ("..".equals(relPathParts[i])) {
-                trimRel += 1; trimRoot += 1;
-            }
-        }
-
-        String [] outputParts = new String[rootPathParts.length + relPathParts.length - trimRoot - trimRel];
-        System.arraycopy(rootPathParts, 0, outputParts, 0, rootPathParts.length - trimRoot);
-        System.arraycopy(relPathParts,
-                trimRel,
-                outputParts,
-                rootPathParts.length - trimRoot,
-                relPathParts.length - trimRel);
-
-        return StringUtils.join(outputParts, "/");
+      try {
+          int until = rootPath.lastIndexOf("/")+1;
+          String root = rootPath.substring(0, until);
+          URL rootUrl = new URL(root);
+          URL finalUrl = new URL(rootUrl, relativePath);
+          return finalUrl.toString();
+      }
+      catch(Exception e) {
+          throw new RuntimeException(e);
+      }
     }
 
     public static String readExternalRef(String file, RefFormat refFormat, List<AuthorizationValue> auths,
