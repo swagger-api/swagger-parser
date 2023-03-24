4423a854b44adfa5c13566db611a92a054d7313d
Yehor Kovalchuk <yehor@wiremock.io>
2023-03-24 13:53:51 +0300

    Revert "Code improvements"
    This reverts commit 93a77c9b

--- modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/processors/SchemaProcessor.java
+++ modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/processors/SchemaProcessor.java
@@ -2,7 +2,6 @@ package io.swagger.v3.parser.processors;
 
 
 import io.swagger.v3.oas.models.OpenAPI;
-
 import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.ComposedSchema;
 import io.swagger.v3.oas.models.media.Discriminator;
@@ -88,6 +87,16 @@ public class SchemaProcessor {
             processDiscriminatorSchema(schema);
         }
 
+        if (doesInternalSchemaContains(schema)) {
+            processInternalPropertyReferences(schema.getItems().getItems());
+        }
+    }
+
+    private boolean doesInternalSchemaContains(Schema schema) {
+        return schema.getItems() != null
+                && schema.getItems().getItems() != null
+                && schema.getItems().getItems().get$ref() != null
+                && schema.getItems().getItems().get$ref().startsWith("..");
     }
 
     private void processDiscriminatorSchema(Schema schema) {
@@ -106,7 +115,7 @@ public class SchemaProcessor {
             if (schema.getAdditionalProperties() != null && schema.getAdditionalProperties() instanceof Schema) {
                 Schema additionalPropertiesSchema = (Schema) schema.getAdditionalProperties();
                 if (additionalPropertiesSchema.get$ref() != null) {
-                    processReferenceSchema(additionalPropertiesSchema);
+                    processReferenceSchemaForProperty(additionalPropertiesSchema);
                 } else {
                     processSchemaType(additionalPropertiesSchema);
                 }
@@ -135,12 +144,16 @@ public class SchemaProcessor {
              for (Map.Entry<String, Schema> propertyEntry : properties.entrySet()) {
                  Schema property = propertyEntry.getValue();
                  if(property.get$ref() != null) {
-                     processReferenceSchema(property);
+                     processReferenceSchemaForProperty(property);
                  }else {
                      processSchemaType(property);
                  }
              }
          }
+    }
+
+    private void processInternalPropertyReferences(Schema schema) {
+        processReferenceSchemaForProperty(schema);
     }
 
     public void processComposedSchema(ComposedSchema composedSchema) {
@@ -234,11 +247,22 @@ public class SchemaProcessor {
 
         if (isAnExternalRefFormat(refFormat)){
             final String newRef = externalRefProcessor.processRefToExternalSchema($ref, refFormat);
-
             if (newRef != null) {
                 schema.set$ref(RefType.SCHEMAS.getInternalPrefix() + newRef);
             }
         }
     }
 
+    private void processReferenceSchemaForProperty(Schema schema) {
+        RefFormat refFormat = computeRefFormat(schema.get$ref());
+        String $ref = schema.get$ref();
+
+        final String newRef = externalRefProcessor.processRefToExternalSchema($ref, refFormat);
+        if (newRef != null && !newRef.startsWith("#/components")) {
+            schema.set$ref(RefType.SCHEMAS.getInternalPrefix() + newRef);
+        }
+        if (schema.getItems() != null && schema.getItems().get$ref().startsWith("..")) {
+            processReferenceSchemaForProperty(schema.getItems());
+        }
+    }
 }
