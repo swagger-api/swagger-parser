7098fc021f5c2b3daeac2dd64ef77cbf1f5f5d87
Martin Lopatář <lop_in_github@dataplex.cz>
2023-08-06 23:37:04 +0200

    Fix #1955 Schema refs of array parameters are not resolved for renamed schema components

--- modules/swagger-parser-v3/src/test/java/io/swagger/v3/parser/processors/PathsProcessorTest.java
+++ modules/swagger-parser-v3/src/test/java/io/swagger/v3/parser/processors/PathsProcessorTest.java
@@ -4,13 +4,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.PathItem.HttpMethod;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.parser.OpenAPIV3Parser;
+import io.swagger.v3.parser.core.models.ParseOptions;
+import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import org.testng.annotations.Test;
 
+import java.net.URL;
 import java.util.Map.Entry;
+import java.util.Objects;
 
 import static java.lang.String.format;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 public class PathsProcessorTest {
 
@@ -41,6 +48,36 @@ public class PathsProcessorTest {
 
         assertOperationsHasParameters(openAPI, "/ref/test/{id}/operationlevelparam");
     }
+
+    @Test
+    public void testProcessPaths_refsToRenamedSchemasResolved() {
+		URL mergeSpecLocation = getClass().getClassLoader().getResource("issue-1955/merged_spec12.yaml");
+		Objects.requireNonNull(mergeSpecLocation);
+		ParseOptions parseOptions = new ParseOptions();
+		parseOptions.setResolve(true);
+
+		SwaggerParseResult swaggerParseResult = new OpenAPIV3Parser().readLocation(mergeSpecLocation.toString(), null, parseOptions);
+
+		swaggerParseResult.getOpenAPI()
+				.getPaths().values().stream()
+				.flatMap(path -> path.readOperationsMap().values().stream())
+				.flatMap(operation -> operation.getParameters().stream())
+				.map(Parameter::getSchema)
+				.forEach(this::assertSchemaNoExternalRefs);
+	}
+
+	private void assertSchemaNoExternalRefs(Schema<?> schema) {
+		if (schema.get$ref() != null) {
+			assertSchemaRefInternal(schema.get$ref());
+		}
+		if (schema.getItems() != null && schema.getItems().get$ref() != null) {
+			assertSchemaRefInternal(schema.getItems().get$ref());
+		}
+	}
+
+	private void assertSchemaRefInternal(String ref) {
+		assertTrue(ref.startsWith("#"));
+	}
 
     private void assertOperationsHasParameters(OpenAPI openAPI, String path) {
         PathItem pathItem = openAPI.getPaths().get(path);
