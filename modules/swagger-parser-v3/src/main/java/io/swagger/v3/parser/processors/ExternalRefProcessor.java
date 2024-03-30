6027e7364a399224d67dc9623b6330fbd78fcb62
chimmi <3124852+chimmi@users.noreply.github.com>
2024-03-30 15:11:00 +0300

    Issue 2071. Correctly handle external refs in array items composed schemas

--- modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/processors/ExternalRefProcessor.java
+++ modules/swagger-parser-v3/src/main/java/io/swagger/v3/parser/processors/ExternalRefProcessor.java
@@ -147,36 +147,7 @@ public final class ExternalRefProcessor {
 
             if(schema instanceof ComposedSchema){
                 ComposedSchema composedSchema = (ComposedSchema) schema;
-                if (composedSchema.getAllOf() != null){
-                    for(Schema item : composedSchema.getAllOf()){
-                        if (item.get$ref() != null){
-                            processRefSchema(item,file);
-                        } else{
-                            processSchema(item, file);
-                        }
-                    }
-
-                }if (composedSchema.getOneOf() != null){
-                    for(Schema item : composedSchema.getOneOf()){
-                        if (item.get$ref() != null){
-                            if (item.get$ref() != null){
-                                processRefSchema(item,file);
-                            }else{
-                                processSchema(item, file);
-                            }
-                        }
-                    }
-                }if (composedSchema.getAnyOf() != null){
-                    for(Schema item : composedSchema.getAnyOf()){
-                        if (item.get$ref() != null){
-                            if (item.get$ref() != null){
-                                processRefSchema(item,file);
-                            }else{
-                                processSchema(item, file);
-                            }
-                        }
-                    }
-                }
+                processComposedSchema(composedSchema, file);
             }
             //Loop the properties and recursively call this method;
             Map<String, Schema> subProps = schema.getProperties();
@@ -212,12 +183,50 @@ public final class ExternalRefProcessor {
                 ArraySchema arraySchema = (ArraySchema) schema;
                 if (StringUtils.isNotBlank(arraySchema.getItems().get$ref())) {
                     processRefSchema(((ArraySchema) schema).getItems(), file);
+                } else if (arraySchema.getItems() instanceof ComposedSchema) {
+                    ComposedSchema composedSchema = (ComposedSchema) arraySchema.getItems();
+                    processComposedSchema(composedSchema, file);
                 } else {
-                    processProperties(arraySchema.getItems().getProperties() ,file);
+                    processProperties(arraySchema.getItems().getProperties(), file);
                 }
             }
         }
         return newRef;
+    }
+
+    private void processComposedSchema(ComposedSchema composedSchema, String file) {
+        if (composedSchema.getAllOf() != null) {
+            for (Schema item : composedSchema.getAllOf()) {
+                if (item.get$ref() != null) {
+                    processRefSchema(item, file);
+                } else {
+                    processSchema(item, file);
+                }
+            }
+
+        }
+        if (composedSchema.getOneOf() != null) {
+            for (Schema item : composedSchema.getOneOf()) {
+                if (item.get$ref() != null) {
+                    if (item.get$ref() != null) {
+                        processRefSchema(item, file);
+                    } else {
+                        processSchema(item, file);
+                    }
+                }
+            }
+        }
+        if (composedSchema.getAnyOf() != null) {
+            for (Schema item : composedSchema.getAnyOf()) {
+                if (item.get$ref() != null) {
+                    if (item.get$ref() != null) {
+                        processRefSchema(item, file);
+                    } else {
+                        processSchema(item, file);
+                    }
+                }
+            }
+        }
     }
 
     private void processSchema(Schema property, String file) {
